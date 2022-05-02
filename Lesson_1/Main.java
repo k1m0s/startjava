7c5153e4877accc3f03765b2f0d6f94a69b95b3d
k1m0s <cucbku1@gmail.com>
2022-05-02 16:34:45 +0300

    implement HW_1

--- Lesson_1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-    int sum = 1 + 2;
-    int sum1 = sum + 3;
-    int sum2 = sum1 + 4;
-    int result = sum2 + 5;
-    System.out.println(result);
-    }
-}
