496ce140d09db4e201b92950bbd8ae9b3be916a8
k1m0s <cucbku1@gmail.com>
2022-11-19 16:57:38 +0300

    implement HW_1

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -114,67 +114,66 @@ public class CyclesTheme {
             System.out.println("\n" + "$");
         }
         dollar++;
-    } while (dollar <= 8);
+        } while (dollar <= 8);
 
-    System.out.println("\n7-Отображение ASCII-символов.");
-    System.out.println("DEC" + " CHAR");
-    for (int i = 0; i <= 127; i++) {
-        if((char) i < '0' && (i % 2) != 0) {
-            System.out.printf("%3s %4s %n", i, (char) i);
+        System.out.println("\n7-Отображение ASCII-символов.");
+        System.out.println("DEC" + " CHAR");
+        for (int i = 0; i <= 127; i++) {
+            if((char) i < '0' && (i % 2) != 0) {
+                System.out.printf("%3s %4s %n", i, (char) i);
+            }
+            if ((char) i > 'a' && (char) i < 'z' && (i % 2) == 0) {
+                System.out.printf("%3s %4s %n", i, (char) i);
+            }
         }
-        if ((char) i > 'a' && (char) i < 'z' && (i % 2) == 0) {
-            System.out.printf("%3s %4s %n", i, (char) i);
+
+        System.out.println("\n8-Проверка, является ли число палиндромом.");
+        int numInput = 1234321;
+        int reverse = 0;
+        int polidrom = numInput;
+        while (numInput != 0) {
+            int remainder = numInput % 10;
+            reverse = reverse * 10 + remainder;
+            numInput = numInput / 10;
         }
-    }
+        if (polidrom == reverse) {
+            System.out.println("Число 1234321 является палиндромом.");
+        } else {
+            System.out.println("Число 1234321 не является палиндромом.");
+        }
 
-    System.out.println("\n8-Проверка, является ли число палиндромом.");
-    int numInput = 1234321;
-    int reverse = 0;
-    int polidrom = numInput;
-    while (numInput != 0) {
-        int remainder = numInput % 10;
-        reverse = reverse * 10 + remainder;
-        numInput = numInput / 10;
-    }
-    if (polidrom == reverse) {
-        System.out.println("Число 1234321 является палиндромом.");
-    } else {
-        System.out.println("Число 1234321 не является палиндромом.");
-    }
+        System.out.println("\n9-Определение, является ли число счастливым.");
+        int num = 123123;
+        int right = num % 1000;
+        int left = num / 1000;
+        int sumRight = 0;
+        int sumLeft = 0;
+        for (int i = 3; i > 0; i--) {
+            sumRight += right % 10;
+            right /= 10;
+            sumLeft += left % 10;
+            left /= 10;
+        }
+        System.out.println("Сумма первых трех = " + sumRight);
+        System.out.println("Сумма последних трех = " + sumLeft);
+        if (sumRight == sumLeft) {
+            System.out.println("Число счастливое ");
+        } else {
+            System.out.println("Число не счастливое ");
+        }
 
-    System.out.println("\n9-Определение, является ли число счастливым.");
-    int num = 123123;
-    int right = num % 1000;
-    int left = num / 1000;
-    int sumRight = 0;
-    int sumLeft = 0;
-    for (int i = 3; i > 0; i--) {
-        sumRight += right % 10;
-        right /= 10;
-        sumLeft += left % 10;
-        left /= 10;
-    }
-    System.out.println("Сумма первых трех = " + sumRight);
-    System.out.println("Сумма последних трех = " + sumLeft);
-    if (sumRight == sumLeft) {
-        System.out.println("Число счастливое ");
-    } else {
-        System.out.println("Число не счастливое ");
-    }
-
-    System.out.println("\n10-Вывод таблицы умножения Пифагора.");
-    System.out.print("_|");
-    for (int i = 2; i < 10; i++){
-        System.out.printf("%3s", "__" + i);
-    }
-    System.out.println("");
-    for (int j = 2; j < 10; j++){
-        System.out.printf("%1s", j + "|");
-        for (int k = 2; k < 10; k++) {
-            System.out.printf("%3s", j * k);
+        System.out.println("\n10-Вывод таблицы умножения Пифагора.");
+        System.out.print("_|");
+        for (int i = 2; i < 10; i++){
+            System.out.printf("%3s", "__" + i);
         }
         System.out.println("");
-    }
-
+        for (int j = 2; j < 10; j++){
+            System.out.printf("%1s", j + "|");
+            for (int k = 2; k < 10; k++) {
+                System.out.printf("%3s", j * k);
+            }
+            System.out.println("");
+        }
     }
 }
