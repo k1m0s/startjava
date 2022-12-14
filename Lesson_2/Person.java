44e405e05dc5bcb16be73afd67d7c8bf16ec2c4c
k1m0s <cucbku1@gmail.com>
2022-12-14 12:36:16 +0300

    implement HW_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "Мужской";
+    String name = "Дима";
+    double height = 1.85;
+    int weight = 80;
+    int age = 30;
+
+    void move() {
+        System.out.println("Идти");
+    }
+
+    void sit() {
+        System.out.println("Сидеть");
+    }
+
+    void run() {
+        System.out.println("Бегать");
+    }
+
+    void talk() {
+        System.out.println("Говорить");
+    }
+
+    void learnJava() {
+        System.out.println("Учить Java");
+    }
+}
