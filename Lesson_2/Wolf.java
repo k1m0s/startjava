ab510cbf021da2527575f4da89ef9c60f4244bd2
k1m0s <cucbku1@gmail.com>
2022-12-24 13:47:14 +0300

    implement HW_2

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -6,6 +6,49 @@ public class Wolf {
     private int age;
     private String color;
 
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        }
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public void walk() {
         System.out.println("Идет");
     }
@@ -24,48 +67,5 @@ public class Wolf {
 
     public void hunt() {
         System.out.println("Охотится");
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setAge(int age) {
-        if (age > 8) {
-            System.out.println("Некорректный возраст");
-        }
-        this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
     }
 }
